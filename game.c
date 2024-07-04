8de60b437aa0af94bfd25fce3171eaac57d24f93
mittiii <srp5628comust@gmail.com>
2024-07-04 16:45:56 +0900

    User name added

--- game.c
+++ game.c
@@ -3,35 +3,17 @@
 #include<string.h>
 #include <time.h>
 
-int coin_toss(){
-    if (rand()%2==0){
-        return 0;
-    }else{
-        return 1;
-    }
+void print_name(){
+    char name[100];
+
+    scanf("%s",name);
+    printf("Hello %s!\n",name);
 }
 
 int main(){
-    int i,count=0,heads_count=0,tails_count=0;
-    char coin[6];
+    printf("Who are you?\n> ");
 
-    srand(time(NULL));
-
-    printf("Tossing a coin...\n");
-
-    for(i=0;i<3;i++){
-        if(coin_toss()==0){
-            strcpy(coin,"Heads");
-            heads_count++;
-        }else{
-            strcpy(coin,"Tails");
-            tails_count++;
-        }
-
-        printf("Round %d: %s\n",i+1,coin);
-    }
-
-    printf("Heads: %d, Tails: %d\n",heads_count,tails_count);
+    print_name();
 
     return 0;
 }
